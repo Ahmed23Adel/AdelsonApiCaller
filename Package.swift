cae5ca88eb4283f3e6250117697fcb297d8375df
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-02-01 09:15:49 +0100

    setting min ios to 16

--- Package.swift
+++ Package.swift
@@ -2,11 +2,12 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
 let package = Package(
     name: "AdelsonApiCaller",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AdelsonApiCaller",
             targets: ["AdelsonApiCaller"]
@@ -16,11 +17,8 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.2")),
         .package(url: "https://github.com/Ahmed23Adel/AdelsonAuthManager", branch: "master"),
         .package(url: "https://github.com/Ahmed23Adel/AdelsonValidator", branch: "main"),
-
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AdelsonApiCaller",
             dependencies: ["Alamofire","AdelsonAuthManager","AdelsonValidator"]
